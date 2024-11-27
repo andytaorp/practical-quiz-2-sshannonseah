4ce459507dcf5b65ffb56edaa423fa0fea81963f
sshannonseah <23018646@myrp.edu.sg>
2024-11-27 15:30:26 +0800

    commit

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,19 @@
 import React from "react";
 
-/**
- * TODO: implement the Task component
- * @function Task
- * @returns {React.Component} A <li> containing the task description and a checkbox to toggle completion
- */
-export default function Task() {}
+function Task({ task, onToggleTask, onDeleteTask }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={task.completed}
+        onChange={() => onToggleTask(task.id, { ...task, completed: !task.completed })}
+      />
+      <span style={{ textDecoration: task.completed ? "line-through" : "none" }}>
+        {task.description}
+      </span>
+      <button onClick={() => onDeleteTask(task.id)}>Delete</button>
+    </li>
+  );
+}
+
+export default Task;
